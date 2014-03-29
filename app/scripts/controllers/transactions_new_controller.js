b5f28ba3d9cb32265b8159588be930396c6900d1
Michael Kohler <mkohler@picobudget.com>
2014-03-29 13:02:02 +0100

    new transaction form implemented

--- app/scripts/controllers/transactions_new_controller.js
+++ app/scripts/controllers/transactions_new_controller.js
@@ -1,2 +1,22 @@
-PicoCash.TransactionsNewController = Ember.ArrayController.extend({
+PicoCash.TransactionsNewController = Ember.ObjectController.extend({
+
+    actions: {
+        saveTransaction: function() {
+            var selectedCategoryID = document.querySelector('#category').value;
+            var that = this;
+            this.store.find('category', selectedCategoryID).then(function (selectedCategory) {
+                that.send('createRecord', selectedCategory);
+            });
+        },
+        createRecord: function(selectedCategory) {
+            console.log('createRecord: ' + this.store);
+            this.store.createRecord('transaction', {
+                name: this.get('name'),
+                amount: parseFloat(this.get('amount')),
+                createDate: Date.now(),
+                category: selectedCategory
+            });
+            this.transitionToRoute('transactions.index');
+        }
+    }
 });
--- app/scripts/routes/transactions_new_route.js
+++ app/scripts/routes/transactions_new_route.js
@@ -1,10 +1,11 @@
 PicoCash.TransactionsNewRoute = Ember.Route.extend({
     model: function() {
-        return this.store.find('transaction');
+        return {};
     },
 
     afterModel: function(transition) {
         var transactionNewController = this.controllerFor('transactions.new');
+        transactionNewController.set('model', {});
         if(Ember.isEmpty(transactionNewController.get('categories'))) {
             this.store.find('category').then(function(categories) {
                 transactionNewController.set('categories', categories);
