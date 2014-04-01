bf0f7fb578f921e6583a31bc27f210ceebc65ceb
Michael Kohler <mkohler@picobudget.com>
2014-04-01 22:17:01 +0200

    try to get transactions in category detail view (not working yet)

--- app/scripts/routes/category_route.js
+++ app/scripts/routes/category_route.js
@@ -1,5 +1,14 @@
 PicoCash.CategoryRoute = Ember.Route.extend({
     model: function(params) {
         return this.store.find('category', params.category_id);
+    },
+
+    afterModel: function(transition) {
+        var categoryController = this.controllerFor('category');
+        if(Ember.isEmpty(categoryController.get('transactions'))) {
+            this.store.find('transactions').then(function(transactions) {
+                categoryController.set('transactions', transactions);
+            });
+        }
     }
 });
