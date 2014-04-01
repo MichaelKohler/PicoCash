f7ad3085e43612907c47c5b0bb7737ccbf2543a5
Michael Kohler <mkohler@picobudget.com>
2014-04-02 01:09:39 +0200

    add Firefox OS WebApp manifest

--- Gruntfile.js
+++ Gruntfile.js
@@ -270,7 +270,8 @@ module.exports = function (grunt) {
                         '*.{ico,txt}',
                         '.htaccess',
                         'images/{,*/}*.{webp,gif}',
-                        'styles/fonts/*'
+                        'styles/fonts/*',
+                        '*.webapp'
                     ]
                 }]
             }
